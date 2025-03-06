558a30cce65a93886b4bc8158fba678f0e541874
Leticia Pieraerts <l.pieraerts@hotmail.com>
2025-03-06 18:41:55 +0100

    commit EOD with Python path commands

--- file.py
+++ file.py
@@ -1,3 +1,22 @@
+from pathlib import Path
+
+# Notebook
+this_dir = Path(".")
+print(this_dir)
+
+this_dir = this_dir.resolve()
+print(this_dir)
+
+root = this_dir.parent
+print(root)
+
+# Py file
+this_file = Path(__file__)
+print(this_file)
+
+root = this_file.parent.parent
+print(root)
+
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     print(x)
